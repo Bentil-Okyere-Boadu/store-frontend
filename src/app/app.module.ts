88f8b291ba54959555acade721ec0067c41236a3
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2023-02-12 17:09:50 +0000

    Storefront Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
