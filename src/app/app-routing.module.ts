79114677fa695f3c8b648b7af983c8917508d132
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2023-02-15 08:24:51 +0000

    setting up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'product', component: ProductDetailsComponent},
+  {path: 'confirmation', component: ConfirmationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
